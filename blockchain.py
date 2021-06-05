b4ba4f22651ec3762a2e53267b37f42a03ce1f31
Maksym <maksym.sh@upplabs.com>
2021-06-05 21:48:01 +0300

    reworking-add-transaction

--- blockchain.py
+++ blockchain.py
@@ -1,30 +1,42 @@
 blockchain = [[1]]
+open_transactions = []
+
+owner = "Maksym"
 
 
 def get_last_blockchain_value():
     # add IDE descriptions
-    """ returns last blockchain value """
+    """returns last blockchain value"""
     if len(blockchain) < 1:
         return None
     return blockchain[-1]
 
 
-def add_value(amount, last_transaction=[1]):
-   # add IDE descriptions
-    """ returns last blockchain value
+def add_transaction(sender, recipient, amount=1.0):
+    # add IDE descriptions
+    """returns last blockchain value
 
     Arguments:
-      :amount: The amount
-      :last_transaction: The last blockchain transaction
+      :sender: The sender of coins
+      :recipient: The recipient of coins
+      :amount: The amount of coins
+
     """
-    if last_transaction == None:
-        last_transaction = [1]
-    blockchain.append([last_transaction, amount])
+    transaction = {"sender": sender, "recipient": recipient, "amount": amount}
+
+    open_transactions.append(transaction)
 
 
-def input_amount():
-    tx_amount = input('Please enter amount: ')
-    add_value(float(tx_amount), get_last_blockchain_value())
+def mine_block():
+    pass
+
+
+def input_transaction_details():
+    tx_recipient = input("Please enter recipient name: ")
+    tx_amount = float(input("Please enter amount: "))
+
+    # add_transaction(owner, tx_recipient, float(tx_amount))
+    return tx_recipient, tx_amount
 
 
 def verify_chain():
@@ -45,26 +57,26 @@ def verify_chain():
 
 # print(blockchain)
 while True:
-    print('Make a choice: ')
-    print('1: Add a new transaction value')
-    print('2: Output the new blockchain blocks')
-    print('h: Manipulate the chain')
-    print('q: Quit')
-    user_choice = input('Your choice: ')
+    print("Make a choice: ")
+    print("1: Add a new transaction value")
+    print("2: Output the new blockchain blocks")
+    print("h: Manipulate the chain")
+    print("q: Quit")
+    user_choice = input("Your choice: ")
 
-    if user_choice == '1':
-        input_amount()
-    elif user_choice == '2':
+    if user_choice == "1":
+        input_transaction_details()
+    elif user_choice == "2":
         for block in blockchain:
             print(block)
-    elif user_choice == 'h':
+    elif user_choice == "h":
         if len(blockchain) >= 1:
             blockchain[0] = [2]
-    elif user_choice == 'q':
+    elif user_choice == "q":
         break
     else:
-        print('Incorrect choice')
+        print("Incorrect choice")
 
     if not verify_chain():
-        print('Not valid blockchain')
+        print("Not valid blockchain")
         break
