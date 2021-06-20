6d345916e3e3e01603320428ca8dec3f16930ff9
Maksym <maksym.sh@upplabs.com>
2021-06-20 20:37:11 +0300

    read-file

--- files/files.py
+++ files/files.py
@@ -1,3 +1,14 @@
-file = open('demo.txt', mode='w')
-file.write('PYTHON')
+file = open("demo.txt", mode="w")
+file.write("PYTHON")
+file.close()
+
+
+file = open("demo.txt", mode="a")
+file.write("is cool")
+file.close()
+
+
+file = open("demo.txt")
+file_data = file.read()
+print(file_data)
 file.close()
