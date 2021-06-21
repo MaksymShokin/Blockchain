19467052cc3e96fde536fe4faa41f3f91b8a55ce
Maksym <maksym.sh@upplabs.com>
2021-06-21 17:30:21 +0300

    with-keyword

--- files/files.py
+++ files/files.py
@@ -14,11 +14,17 @@ print(file_data)
 file.close()
 
 
+# closes file automatically
+with open("demo.txt") as t:
+    file_data = file.read()
+    print(file_data)
+
+
 # store multiline
 # file.write("PYTHON\n")
 
 # read multiline
-# file.readlines() 
+# file.readlines()
 # returns list of strings
 
 # read single line
