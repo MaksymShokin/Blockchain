651500c506ac13cb92ca0fbc034fe3b8ef02b97e
Maksym <maksym.sh@upplabs.com>
2021-06-28 00:19:36 +0300

    class-constuctors

--- oop/car.py
+++ oop/car.py
@@ -1,16 +1,23 @@
 class Car:
-  top_speed = 100
+    # top_speed = 100
 
-  def driving(self):
-    print(f"I am driving at maximum speed of {self.top_speed}")
+    def __init__(self, top_speed=100) -> None:
+        self.top_speed = top_speed
+        self.warnings = []
+
+    def driving(self):
+        print(f"I am driving at maximum speed of {self.top_speed}")
 
 
 car1 = Car()
 car1.driving()
 
-dict = {
-  "attr": 100,
-  "met": lambda: print("method called")
-}
+car2 = Car()
+car2.driving()
+
+car3 = Car()
+car3.driving()
+
+dict = {"attr": 100, "met": lambda: print("method called")}
 
 print(dict["met"]())
