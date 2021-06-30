d4be0339364428d17102ab104d2f0ff8f480e428
Maksym <maksym.sh@upplabs.com>
2021-06-30 16:05:57 +0300

    private-properties

--- oopBlockchain/blockchain.py
+++ oopBlockchain/blockchain.py
@@ -20,10 +20,16 @@ class Blockchain:
     def __init__(self, hosting_node_id) -> None:
         GENESIS_BLOCK = Block(0, "", [], 100, 0)
 
-        self.chain = [GENESIS_BLOCK]
-        self.open_transactions = []
+        self.__chain = [GENESIS_BLOCK]
+        self.__open_transactions = []
         self.load_data()
         self.hosting_node = hosting_node_id
+
+    def get_blockchain(self):
+      return self.__chain[:]
+
+    def get_open_transactions(self):
+      return self.__open_transactions[:]
 
     def load_data(self):
         try:
@@ -42,7 +48,7 @@ class Blockchain:
                     )
                     updated_blockchain.append(updated_block)
 
-                self.chain = updated_blockchain
+                self.__chain = updated_blockchain
                 open_transactions = json.loads(file_content[1])
                 updated_transactions = []
 
@@ -67,7 +73,7 @@ class Blockchain:
                             block_el.proof,
                             block_el.timestamp,
                         )
-                        for block_el in self.chain
+                        for block_el in self.__chain
                     ]
                 ]
 
@@ -80,7 +86,7 @@ class Blockchain:
             print("Save data fail")
 
     def proof_of_work(self):
-        last_block = self.chain[-1]
+        last_block = self.__chain[-1]
         last_hash = hash_block(last_block)
 
         proof = 0
@@ -93,13 +99,13 @@ class Blockchain:
     def get_balance(self):
         participant = self.hosting_node
 
-        tx_sender = [[tx.amount for tx in block.transactions if tx.sender == participant] for block in self.chain]
+        tx_sender = [[tx.amount for tx in block.transactions if tx.sender == participant] for block in self.__chain]
         tx_sender_open_transactions = [tx.amount for tx in self.open_transactions if tx.sender == participant]
         tx_sender.append(tx_sender_open_transactions)
 
         amount_sent = reduce(lambda acc, val: acc + sum(val) if len(val) > 0 else acc + 0, tx_sender, 0)
 
-        tx_recipient = [[tx.amount for tx in block.transactions if tx.recipient == participant] for block in self.chain]
+        tx_recipient = [[tx.amount for tx in block.transactions if tx.recipient == participant] for block in self.__chain]
 
         amount_received = reduce(lambda acc, val: acc + sum(val) if len(val) > 0 else acc + 0, tx_recipient, 0)
         print(amount_received - amount_sent)
@@ -108,9 +114,9 @@ class Blockchain:
     def get_last_blockchain_value(self):
         # add IDE descriptions
         """returns last blockchain value"""
-        if len(self.chain) < 1:
+        if len(self.__chain) < 1:
             return None
-        return self.chain[-1]
+        return self.__chain[-1]
 
     def add_transaction(self, recipient, sender=owner, amount=1.0):
         # add IDE descriptions
@@ -133,7 +139,7 @@ class Blockchain:
         return False
 
     def mine_block(self):
-        hashed_block = hash_block(self.chain[-1])
+        hashed_block = hash_block(self.__chain[-1])
 
         proof = self.proof_of_work()
 
@@ -142,9 +148,9 @@ class Blockchain:
         copied_transactions = self.open_transactions[:]
         copied_transactions.append(reward_transaction)
 
-        block = Block(len(self.chain), hashed_block, copied_transactions, proof)
+        block = Block(len(self.__chain), hashed_block, copied_transactions, proof)
 
-        self.chain.append(block)
+        self.__chain.append(block)
         self.open_transactions = []
         self.save_data()
         return True
--- oopBlockchain/node.py
+++ oopBlockchain/node.py
@@ -36,14 +36,14 @@ class Node:
                 else:
                     print("Transaction failed")
 
-                print(self.blockchain.open_transactions)
+                print(self.blockchain.get_open_transactions())
             elif user_choice == "2":
                 self.blockchain.mine_block()
             elif user_choice == "3":
-                for block in self.blockchain.chain:
+                for block in self.blockchain.get_blockchain():
                     print(block)
             elif user_choice == "4":
-                if Verification.verify_transactions(self.blockchain.open_transactions, self.blockchain.get_balance):
+                if Verification.verify_transactions(self.blockchain.get_open_transactions(), self.blockchain.get_balance):
                     print("All transactions are valid")
                 else:
                     print("Some transactions are malformed")
@@ -53,7 +53,7 @@ class Node:
                 print("Incorrect choice")
 
             print("Balance of {}: {:6.2f}".format(self.id, float(self.blockchain.get_balance())))
-            if not Verification.verify_chain(self.blockchain.chain):
+            if not Verification.verify_chain(self.blockchain.get_blockchain()):
                 print("Not valid blockchain")
                 break
 
